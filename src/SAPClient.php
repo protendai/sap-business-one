fabbb67e8a23b70c1299cef82786dc246f2dd369
protendai <protendai@gmail.com>
2022-05-28 00:00:37 +0200

    Fixed config file issue

--- src/SAPClient.php
+++ src/SAPClient.php
@@ -14,8 +14,9 @@ class SAPClient{
     /**
      * Initializes SAPClient with configuration and session data.
      */
-    public function __construct(array $configOptions, array $session){
-        $this->config = new Config($configOptions);
+    public function __construct(array $session){
+        $configOptions =  config('sap.sap'); 
+        $this->config  = new Config($configOptions);
         $this->session = $session;
     }
     
@@ -44,8 +45,9 @@ class SAPClient{
      * Creates a new SAP B1 session and returns a new instance of SAPb1\Client.
      * Throws SAPb1\SAPException if an error occurred.
      */
-    public static function createSession(array $configOptions, string $username, string $password, string $company) : SAPClient{
-        
+    public static function createSession(string $username, string $password, string $company) : SAPClient{
+
+        $configOptions =  config('sap.sap'); 
         $config = new Config($configOptions);
 
         $request = new Request($config->getServiceUrl('Login'), $config->getSSLOptions());
--- src/SapBusinessOneServiceProvider.php
+++ src/SapBusinessOneServiceProvider.php
@@ -9,14 +9,7 @@ class SapBusinessOneServiceProvider extends ServiceProvider
 {
   public function register()
   {
-    // $this->app->bind('SAPClient', function($app) {
-    //     return new SAPClient();
-    // });
-
-    // $this->mergeConfigFrom(
-    //   __DIR__.'/config/sap.php', 'sap'
-    // );
-
+    
     if (file_exists(config_path('sap.php'))) {
         $this->mergeConfigFrom(config_path('sap.php'), 'sap');
     } else {
@@ -32,9 +25,6 @@ class SapBusinessOneServiceProvider extends ServiceProvider
             InstallSapPackage::class,
         ]);
 
-        // $this->publishes([
-        //   __DIR__.'/config/sap.php' => config_path('sap.php'),
-        // ],'config');
         $this->publishes([
           __DIR__ . '/../config/sap.php' => config_path('sap.php'),
       ], 'sap');
